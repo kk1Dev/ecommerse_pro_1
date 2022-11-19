9f2c00e9e10b868f3623c96868627127b6ce8a0e
kk1Dev <kpkundan243@gmail.com>
2022-11-19 09:22:16 +0530

    modified:   configs/.gitignore modified:   configs/server.config.js 	.gitignore 	app.js 	configs/auth.config.js 	controllers/ 	jest.config.js 	middlewares/ 	models/ 	routes/ 	test/

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const serverConfig =require('./configs/server.config');
+const app = express();
+app.listen(serverConfig.PORT, ()=>{console.log(`Application started on port no: ${serverConfig.PORT}`);
+})
+
